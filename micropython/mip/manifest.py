7d2cc1d2534d19767feaada9247d8a902b026243
Carlosgg <carlosgilglez@gmail.com>
2023-02-28 22:52:58 +0000

    micropython/mip: Add async mip.
    	This adds an async mip version compatible with
    	micropython/micropython#8968
    
    Signed-off-by: Carlos Gil <carlosgilglez@gmail.com>

--- micropython/mip/manifest.py
+++ micropython/mip/manifest.py
@@ -1,5 +1,9 @@
-metadata(version="0.2.0", description="On-device package installer for network-capable boards")
+metadata(
+    version="0.2.0",
+    description="On-device package installer for network-capable boards",
+)
 
 require("urequests")
+require("async_urequests")
 
 package("mip", opt=3)
